4c85687a5fcde0aed8b7fc18c7c306584326d3d8
mBuschauer <marco@buschauer.us>
2025-02-13 15:00:24 -0500

    add shell

---
+++ shell.nix
@@ -0,0 +1,14 @@
+{ pkgs ? import <nixpkgs> { } }:
+pkgs.mkShell {
+  buildInputs = with pkgs.buildPackages; [
+    nodejs_23
+  ];
+
+  shellHook =
+    ''
+      npm install
+      clear
+      echo "Environment ready"
+    '';
+
+}
