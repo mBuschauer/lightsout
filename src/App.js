87156d09cf0f3bfd8b7418e2c20d9b7523efe371
mBuschauer <marco@buschauer.us>
2025-02-18 23:10:24 -0500

    add description

--- src/App.js
+++ src/App.js
@@ -4,18 +4,28 @@ import Board from './components/Board/Board';
 import Title from './components/Title/Title';
 
 const App = () => {
-    const [ won, setWon ] = useState(false)
+    const [won, setWon] = useState(false)
 
     useEffect(() => {
         document.title = "Twice Dead - Lights Out"; // Change tab title
-      }, []);
+
+        const metaDescription = document.querySelector("meta[name='description']");
+        if (metaDescription) {
+            metaDescription.setAttribute("content", "Lights Out puzzle for Gehenna's Furnace (Twice Dead - DnD 5e Campaign)");
+        } else {
+            const meta = document.createElement("meta");
+            meta.name = "description";
+            meta.content = "Lights Out puzzle for Gehenna's Furnace (Twice Dead - DnD 5e Campaign)";
+            document.head.appendChild(meta);
+        }
+    }, []);
 
     return (
         <div className="App">
             <Title
                 finished={won}
             />
-            <Board 
+            <Board
                 size={5}
                 finished={won}
                 setFinished={setWon}
