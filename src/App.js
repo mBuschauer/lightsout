4440d1ef0bdd381c95b8031870917112aca8f10e
mBuschauer <marco@buschauer.us>
2025-02-18 23:09:08 -0500

    changed tab name

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import Board from './components/Board/Board';
 import Title from './components/Title/Title';
 
 const App = () => {
     const [ won, setWon ] = useState(false)
+
+    useEffect(() => {
+        document.title = "Twice Dead - Lights Out"; // Change tab title
+      }, []);
 
     return (
         <div className="App">
